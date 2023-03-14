e25186ad55a9e941be4288b0cad682c9e95a15f8
sefaerdoğdugil <s.erdogdugil@gmail.com>
2023-03-14 19:10:53 +0300

    Initial commit

---
+++ src/Deneme.java
@@ -0,0 +1,12 @@
+
+
+public class Deneme {
+    public static void main(String[] args){
+        telefon  tele=new telefon();
+        tele.kameraSayisi=4;
+        tele.marka="oppo";
+        tele.renk="mavi";
+        tele.ram=6;
+
+        System.out.println(tele.kameraSayisi);
+}}
---
+++ src/telefon.java
@@ -0,0 +1,11 @@
+public class telefon {
+
+   public String renk;
+   public int kameraSayisi;
+
+   public int ram;
+
+   public String marka;
+
+
+}
